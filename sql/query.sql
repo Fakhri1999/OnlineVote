45681f05a8fed0dc28adc3f2e431da34a83d5ed8
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2019-04-28 10:36:37 +0700

    add event active

--- sql/query.sql
+++ sql/query.sql
@@ -11,4 +11,17 @@ WHERE p.kode_room = 'RHPOL'
 GROUP BY p.id_pilihan
 
 -- GET DATA FOR FILE
-SELECT r.*, COUNT(v.id_pilihan) AS qty, p.nama_pilihan, (SELECT COUNT(*) FROM voter v2 WHERE v2.kode_room = 'lBESm') AS total FROM pilihan p JOIN room r ON r.kode_room = p.kode_room LEFT JOIN voter v ON p.id_pilihan = v.id_pilihan WHERE p.kode_room = 'lBESm' GROUP BY p.id_pilihan
+SELECT r.*, COUNT(v.id_pilihan) AS qty, p.nama_pilihan, (SELECT COUNT(*)
+   FROM voter v2
+   WHERE v2.kode_room = 'lBESm') AS total
+FROM pilihan p JOIN room r ON r.kode_room = p.kode_room LEFT JOIN voter v ON p.id_pilihan = v.id_pilihan
+WHERE p.kode_room = 'lBESm'
+GROUP BY p.id_pilihan
+
+-- Database room active event
+CREATE EVENT
+IF NOT EXISTS activeExp
+ON SCHEDULE EVERY 1 MINUTE
+DO
+UPDATE room SET active = 0
+   WHERE CURDATE() > waktu_akhir
